a69c4c89321b04dd82cfdfbcbec59cea0990efcd
CrazyTea <lipatow.nikita@yandex.ru>
2019-11-06 14:49:05 +0300

    bug fix

--- commands/ReferenceController.php
+++ commands/ReferenceController.php
@@ -109,16 +109,15 @@ class ReferenceController extends Controller
 
 
             $student = Students::findOne(['name'=>$row[$nameId],'code'=>$row[$codeId],'date_credit'=>$row[$dCreditId]]);
-            if (!$student){
+            if (!$student) {
                 $student = new Students();
+            }
                 $student->date_start = $row[$dStart];
                 $student->name = $row[$nameId];
                 $student->code = $row[$codeId];
                 $student->date_credit = $row[$dCreditId];
-                $org  = Organizations::findOne($row[$orgId]);
-                if ($org)
-                    $student->id_org = $row[$orgId];
-            }
+                $student->id_org = $row[$orgId];
+
             $n = NumbersPp::findOne($row[$numPP]);
             $b = Banks::findOne($row[$bankId]);
             if ($n)
