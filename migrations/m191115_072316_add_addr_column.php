7b1d2bc991a9762b53a20ad6f23c91723ba102f7
CrazyTea <lipatow.nikita@yandex.ru>
2019-11-15 10:25:03 +0300

    parse

---
+++ migrations/m191115_072316_add_addr_column.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191115_072316_add_addr_column
+ */
+class m191115_072316_add_addr_column extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn(\app\models\app\Organizations::tableName(),'addr',$this->text());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m191115_072316_add_addr_column cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191115_072316_add_addr_column cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
