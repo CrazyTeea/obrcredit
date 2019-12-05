5abd2208cf125706f367275f09ddbb63d0820b3c
CrazyTea <lipatow.nikita@yandex.ru>
2019-12-05 15:29:28 +0300

    fix

--- frontAssets/index.js
+++ frontAssets/index.js
@@ -2,7 +2,7 @@ import "./src/styles/index.scss";
 import Router from "./src/Router";
 import students from "./src/routes/students";
 
-$(()=>{
+$(document).ready(()=>{
     $('[data-toggle="tooltip"]').tooltip();
     Router('students').then(()=>{
         students();
