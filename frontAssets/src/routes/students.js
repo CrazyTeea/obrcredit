a35424c697655e162d8298a547ba08f4f11ff1a6
CrazyTea <lipatow.nikita@yandex.ru>
2020-02-06 09:35:44 +0300

    fix

--- frontAssets/src/routes/students.js
+++ frontAssets/src/routes/students.js
@@ -1,25 +1,24 @@
 import Router from "../Router";
 
 export default ()=>{
+    let ed_s = $("#students-education_status");
+    let e_status = document.getElementById('students-education_status');
+    function disable(e_status,check){
 
-
-    Router('update').then(()=>{
-        function disable(e_status,check){
-
-            let elements = document.getElementsByTagName('input');
-            for (let i = 0; i < elements.length; i++) {
-                if (elements[i].type === 'radio'&& elements[i].value !== "0" && elements[i].name ==='Students[osnovanie]'
-                    || (elements[i].type === 'file' && elements[i].name ==='Students[rasp_act0]')
-                    || (elements[i].type === 'file' && elements[i].name ==='Students[dogovor]')
-                    || (elements[i].type === 'file' && elements[i].name ==='Students[rasp_act_otch]')  ) {
-                    elements[i].disabled = check;
-                }
-                if (elements[i].type === 'radio' && elements[i].name ==='Students[osnovanie]' && elements[i].value === "0" && !e_status.value)
-                    elements[i].checked = true;
-
+        let elements = document.getElementsByTagName('input');
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].type === 'radio'&& elements[i].value !== "0" && elements[i].name ==='Students[osnovanie]'
+                || (elements[i].type === 'file' && elements[i].name ==='Students[rasp_act0]')
+                || (elements[i].type === 'file' && elements[i].name ==='Students[dogovor]')
+                || (elements[i].type === 'file' && elements[i].name ==='Students[rasp_act_otch]')  ) {
+                elements[i].disabled = check;
             }
+            if (elements[i].type === 'radio' && elements[i].name ==='Students[osnovanie]' && elements[i].value === "0" && !e_status.value)
+                elements[i].checked = true;
+
         }
-        let ed_s = $("#students-education_status");
+    }
+    Router(['create','update']).then(()=>{
 
         $('div').on('change','.status_callback',function (e) {
             let dis  = true;
@@ -41,11 +40,6 @@ export default ()=>{
         });
 
         let form = document.getElementById('w0');
-        let e_status = document.getElementById('students-education_status');
-
-        if(!ed_s.find(':checked').val())
-            disable(e_status,true);
-        else disable(e_status,false);
 
         $("button[href='#clean']").click(()=>{
             $(":input[name='Students[education_status]']").prop('disabled',false);
@@ -64,10 +58,11 @@ export default ()=>{
                 disable(e_status,true);
         });
 
-        /*if (form.elements['Students[education_status]'].value==1)
-            disable(e_status);
-        e_status.onchange = e=>{
-            disable(e_status);
-        };*/
     }).catch(e=>{console.log(e)});
+    Router('create').then(()=>{
+
+        if(!ed_s.find(':checked').val())
+            disable(e_status,true);
+        else disable(e_status,false);
+    })
 }
