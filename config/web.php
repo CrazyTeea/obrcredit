e34719defd884e42b22c0387da1243c6558d1201
Crazyteea <lipatow.nikita@yandex.ru>
2020-03-26 12:31:21 +0300

    kek

--- config/web.php
+++ config/web.php
@@ -73,7 +73,7 @@ $config = [
         ],
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => false,
+            'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
@@ -90,7 +90,7 @@ $config = [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning','info'],
+                    'levels' => ['error', 'warning',],
                 ],
             ],
         ],
