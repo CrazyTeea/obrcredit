9e830343cd3a300fb14af813171002ca96ef733e
Crazyteea <lipatow.nikita@yandex.ru>
2020-05-02 14:37:25 +0300

    fix

--- views/app/students-admin/update.php
+++ views/app/students-admin/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\StudentsAdmin */
 
-$this->title = 'Update Students Admin: ' . $model->name;
+$this->title = 'Редактирование: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Students Admins', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
